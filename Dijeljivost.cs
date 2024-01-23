218eadfc691a16466e0294a6b79eb3e4fe93c58a
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2024-01-23 12:23:06 +0100

    Add files via upload

---
+++ Dijeljivost.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijeljivost
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double broj = 23;
+
+            if (broj % 4 == 0 && broj % 6 == 0)
+            {
+                Console.Write("Broj je dijeljiv sa 4 i sa 6.");
+            }
+            else if (broj % 4 != 0 && broj % 6 != 0)
+            {
+                Console.Write("Broj nije dijeljiv ni sa 4 ni sa 6.");
+            }
+            else if (broj % 4 == 0)
+            {
+                Console.Write("Broj je dijeljiv sa 4. ");
+            }
+            else if (broj % 6 == 0)
+            {
+                Console.Write("Broj je dijeljiv sa 6.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
